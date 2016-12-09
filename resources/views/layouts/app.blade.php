6ee77ca9d40bc549ae2d8fe0cf7b4264599a59fb
Eric Scuccimarra <skooch@gmail.com>
2016-12-09 17:16:59 +0100

    Updated Google sign in client key

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-	<meta name="google-signin-client_id" content="613285565368-nk0oi3lketil7d03bk1r6igmnc52ssa8.apps.googleusercontent.com">
+	<meta name="google-signin-client_id" content="556265380008-lap52g9fj0qsdn4c47lsdnr7ap3bi1g2.apps.googleusercontent.com">
     
     <title>{{ app_name() }} {{ isset($title) ? ' - ' . $title : '' }}</title>
 
