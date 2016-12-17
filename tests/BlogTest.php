81fd551a877c9cf4b92e72fc93ec7d5df6fec54f
Eric Scuccimarra <skooch@gmail.com>
2016-12-17 17:31:27 +0100

    Updated blog test to match test for skooch blog

--- tests/BlogTest.php
+++ tests/BlogTest.php
@@ -8,172 +8,221 @@ use Illuminate\Support\Facades\Redis;
 class BlogTest extends TestCase
 {
 	use DatabaseTransactions;
-	
-    /**
-     * Test blog pages
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
-    
-    public function testBlogPagesAppear (){
-    	// Check if pages are OK with no user
-    	$this->visit('/blog')
-    		->see('Archives')
-    		->dontSee('Add Blog Post');
-    	
-    	// since I changed middleware so that requests to admin pages return 404 errors instead of redirects, 
-    	// this has been updated accordingly
-    	$response = $this->call('GET', '/blog/create');
-    	$this->assertEquals(404, $response->status());
-    		
-    	// CHeck if pages are OK with a user
-    	$user = factory(App\User::class)->create();
-    	
-    	$this->actingAs($user)
-    		->visit('/blog')
-    		->see('Archives')
-    		->dontSee('Show/Hide')
-    		->dontSee('Add Blog Post');
-    	
-    	$response = $this->call('GET', '/blog/create');
-    	$this->assertEquals(404, $response->status());
-    		
-    	// Check if pages are OK with admin
-    	$user->type = 1;
-    	 
-    	$this->actingAs($user)
-	    	->visit('/blog')
-	    	->see('Archives')
-	    	->see('Add Blog Post');
-	  
-//     	$user->destroy($user->id);
-    }
-    
-    public function testBlogAdmin(){
-    	$user = factory(App\User::class)->create();
-    	$user->type = 1;
-    	$user->save();
-    	
-    	// check that we can make new blogs
-    	$this->actingAs($user)
-    		->visit('/blog/create')
-	    	->see('Write a New Blog Post');
-    	
-	    // make a new blog, check that it is made, edit it, check that it is edited, change the date	
-	    $this->actingAs($user)
-	    	->visit('/blog/create')
-	    	->type('PHP Unit Test', 'title')
-	    	->type('PHP Unit Test', 'slug')
-	    	->type('PHP Unit Test', 'body')
-	    	->select('2', 'tags')
-	    	->press('Add Blog Post')
-	    	->see('Your blog has been created')
-	    	->seePageIs('/blog')
-	    	->see('PHP Unit Test')
-	    	->see('No comments');
-	    
-	    // make sure the new blog appears on the home page
-	    $this->actingAs($user)
-	    	->visit('/home')
-	    	->see('PHP Unit Test');
-	   
-	    // make sure it appears in the RSS feed
-	    $this->visit('/feed')
-	    	->see('PHP Unit Test');
-	    	
-	    // test editing of blog
-	    $this->actingAs($user)
-	    	->visit('/blog')
-	    	->click('PHP Unit Test')
-	    	->see('PHP Unit Test')
-	    	->see('Edit Blog')
-	    	->click('Edit Blog')
-	    	->see('Edit:')
-	    	->type('Test Content', 'body')
-	    	->press('Update Blog')
-	    	->see('Your blog post has been edited!')
-	    	->seePageIs('/blog/php-unit-test')
-	    	->see('PHP Unit Test')
-	    	->click('PHP Unit Test')
-	    	->click('Edit Blog')
-	    	->type('12/31/2019', 'published_at')
-	    	->press('Update Blog')
-	    	->see('Your blog post has been edited!')
-	    	->see('PHP Unit Test');
-    	
-	   	// see that blog exists in DB
-	    $this->seeInDatabase('blogs', [
-	    	'title' => 'PHP Unit Test',	
-	    ]);	
-	    
-	    // make sure the new blog does NOT appears on the home page
-	    $this->actingAs($user)
-	    	->visit('/home')
-	   	 ->dontSee('PHP Unit Test');
-	    
-	    // make sure it doesn't appear in the RSS feed
-	    $this->visit('/feed')
-	    	->dontSee('PHP Unit Test');
-	    
-	    // proper links appear as admin
+
+	/**
+	 * Test blog pages
+	 *
+	 * @return void
+	 */
+	public function testExample()
+	{
+		$this->assertTrue(true);
+	}
+
+	public function testBlogPagesAppear (){
+		// get a random post from the DB
+		$blog = \App\Blog::getAll()->first();
+		 
+		// Check if pages are OK with no user
+		$this->visit('/blog')
+		->see('Archives')
+		->dontSee('Add Blog Post')
+		->see($blog->title);
+		 
+		$this->visit('/blog/' . $blog->slug)
+		->assertResponseOk()
+		->see($blog->title);
+		 
+		// check that people can't see pages they shouldn't be able to
+		$response = $this->call('GET', '/blog/create');
+		$this->assertEquals(404, $response->status());
+
+		$response = $this->call('GET', '/blog/' . $blog->id . '/edit');
+		$this->assertEquals(404, $response->status());
+		 
+		// CHeck if pages are OK with a user
+		$user = factory(App\User::class)->create();
+		 
 		$this->actingAs($user)
-	    	->visit('/blog/php-unit-test')
-	    	->see('PHP Unit Test')
-	    	->see('Edit Blog')
-	    	->see('Delete Blog');
-	    
-	    // check that label is working
-	    $this->actingAs($user)
-	    	->visit('/blog/labels/test')
-	    	->see('PHP Unit Test');
-	    
-	    // see that it doesn't appear for non-admins	
-	    // it also shouldn't appear because the data is cached
-	    $user->type = 0;
-	    $user->save();
-	    
-	    $this->visit('/blog/php-unit-test')
-		    ->dontSee('PHP Unit Test')
-		    ->dontSee('Edit Blog')
-		    ->dontSee('Delete Blog');
-		     
-	    $this->actingAs($user)
-		    ->visit('/blog/php-unit-test')
-		    ->dontSee('PHP Unit Test')
-		    ->dontSee('Edit Blog')
-		    ->dontSee('Delete Blog');
+		->visit('/blog')
+		->see('Archives')
+		->dontSee('Show/Hide')
+		->dontSee('Add Blog Post')
+		->see($blog->title);
+		 
+		$this->actingAs($user)
+		->visit('/blog/' . $blog->slug)
+		->assertResponseOk()
+		->see($blog->title);
+
+		// Check if pages are OK with admin
+		$user->type = 1;
+
+		$this->actingAs($user)
+		->visit('/blog')
+		->see('Archives')
+		->see('Add Blog Post');
+
+		$this->actingAs($user)
+		->visit('/blog/' . $blog->slug)
+		->assertResponseOk()
+		->see($blog->title);
+
+		$this->visit('/blog/create')
+		->assertResponseOk();
+	  
+		$this->visit('/blog/' . $blog->id . '/edit')
+		->assertResponseOk();
+	  
+		$user->destroy($user->id);
+	}
+
+	public function testBlogAdmin(){
+		$user = factory(App\User::class)->create();
+		$user->type = 1;
+		$user->save();
+		 
+		// get id of tag for test
+		$test = \App\Tag::where('name', 'test')->first();
 		
-	    $this->actingAs($user)
-	    	->visit('/blog')
-	    	->dontSee('PHP Unit Test');
-	    	
-	    // change back to admin user, reset date on test article
-	    $user->type = 1;
-	    $user->save();
-	    
-	    $this->actingAs($user)
-	    	->visit('/blog/php-unit-test')
-	    	->click('Edit Blog')
-	    	->type('12/31/2015', 'published_at')
-	    	->press('Update Blog')
-	    	->see('Your blog post has been edited!')
-	    	->see('PHP Unit Test');
-	   
-	    // check that we can see it as no user
-    	$user->type = 0;
-    	
-    	$this->visit('/blog/php-unit-test')
-	    	->see('PHP Unit Test')
-	    	->dontSee('Edit Blog')
-	    	->dontSee('Delete Blog');
-	   
-     	$user->destroy($user->id);
-    	
-     	// flush the redis list
-     	Redis::flushall();
-    }
+		// make a new blog, check that it is made, edit it, check that it is edited, change the date
+		$this->actingAs($user)
+		->visit('/blog/create')
+		->assertResponseOk()
+		->see('Write a New Blog Post')
+		->type('PHP Unit Test', 'title')
+		->type('PHP Unit Test', 'slug')
+		->type('PHP Unit Test', 'body')
+		->select($test->id, 'tags')
+		->press('Add Blog Post')
+		->see('Your blog has been created')
+		->seePageIs('/blog')
+		->see('PHP Unit Test')
+		->see('No comments');
+	  
+		// check that blog page appears
+		$this->actingAs($user)
+		->visit('/blog/php-unit-test')
+		->assertResponseOk()
+		->see('PHP Unit Test')
+		->see('Edit Blog');
+
+		// make sure the new blog appears on the home page
+		$this->actingAs($user)
+		->visit('/home')
+		->see('PHP Unit Test');
+
+		// make sure it appears in the RSS feed
+		$this->visit('/feed')
+		->see('PHP Unit Test');
+
+		// test editing of blog
+		$this->actingAs($user)
+		->visit('/blog')
+		->click('PHP Unit Test')
+		->see('PHP Unit Test')
+		->see('Edit Blog')
+		->click('Edit Blog')
+		->see('Edit:')
+		->type('Test Content', 'body')
+		->press('Update Blog')
+		->see('Your blog post has been edited!')
+		->seePageIs('/blog/php-unit-test')
+		->see('PHP Unit Test')
+		->click('PHP Unit Test')
+		->see('Test Content')
+		->click('Edit Blog')
+		->type('12/31/2026', 'published_at')
+		->press('Update Blog')
+		->see('Your blog post has been edited!')
+		->see('PHP Unit Test');
+		 
+		// see that blog exists in DB
+		$this->seeInDatabase('blogs', [
+				'title' => 'PHP Unit Test',
+		]);
+	  
+		// make sure the new blog does NOT appears on the home page
+		$this->actingAs($user)
+		->visit('/home')
+		->dontSee('PHP Unit Test');
+	  
+		// make sure it doesn't appear in the RSS feed
+		$this->visit('/feed')
+		->dontSee('PHP Unit Test');
+	  
+		// proper links appear as admin
+		$this->actingAs($user)
+		->visit('/blog/php-unit-test')
+		->see('PHP Unit Test')
+		->see('Edit Blog')
+		->see('Delete Blog');
+	  
+		// check that label is working
+		$this->actingAs($user)
+		->visit('/blog/labels/test')
+		->see('PHP Unit Test');
+	  
+		// see that it doesn't appear for non-admins
+		// it also shouldn't appear because the data is cached
+		$user->type = 0;
+		$user->save();
+	  
+		$this->visit('/blog/php-unit-test')
+		->dontSee('PHP Unit Test')
+		->dontSee('Edit Blog')
+		->dontSee('Delete Blog');
+		 
+		$this->actingAs($user)
+		->visit('/blog/php-unit-test')
+		->dontSee('PHP Unit Test')
+		->dontSee('Edit Blog')
+		->dontSee('Delete Blog');
+
+		$this->actingAs($user)
+		->visit('/blog')
+		->dontSee('PHP Unit Test');
+
+		// change back to admin user, reset date on test article
+		$user->type = 1;
+		$user->save();
+	  
+		$this->actingAs($user)
+		->visit('/blog/php-unit-test')
+		->click('Edit Blog')
+		->type('12/31/2015', 'published_at')
+		->press('Update Blog')
+		->see('Your blog post has been edited!')
+		->see('PHP Unit Test');
+
+		// check that we can see it as no user
+		$user->type = 0;
+		 
+		$this->visit('/blog/php-unit-test')
+		->see('PHP Unit Test')
+		->dontSee('Edit Blog')
+		->dontSee('Delete Blog');
+
+		$user->destroy($user->id);
+		 
+		// flush the redis list
+		Redis::flushall();
+	}
+
+	public function testComments(){
+		// create a user
+		$user = factory(App\User::class)->create();
+		// get a random post from the DB
+		$blog = \App\Blog::getAll()->first();
+		 
+		// go to its page and leave a comment
+		$this->actingAs($user)
+		->visit('/blog/' . $blog->slug)
+		->see('Leave a Comment')
+		->click('Leave a Comment')
+		->type('Test comment', 'body')
+		->press('Post Comment')
+		->see('Your comment has been posted')
+		->see('Test comment');
+	}
 }
